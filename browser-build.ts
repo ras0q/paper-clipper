3f6dafcd00f9f3a3fbcc91337081033cecc84b04
ras0q <ras0q@users.noreply.github.com>
2025-02-06 16:02:35 +0900

    fix build script

--- browser-build.ts
+++ browser-build.ts
@@ -1,9 +1,9 @@
 import * as esbuild from "npm:esbuild@0.24.2";
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.11.1";
-import { dirname, fromFileUrl, join } from "jsr:@std/path@1.0.8";
+import { dirname, fromFileUrl, join, relative } from "jsr:@std/path@1.0.8";
 
-const srcDir = dirname(fromFileUrl(import.meta.url));
-const rootDir = join(srcDir, "../");
+const rootDir = relative(Deno.cwd(), dirname(fromFileUrl(import.meta.url)));
+const srcDir = join(rootDir, "browser-extension");
 const distDir = join(rootDir, "dist");
 await Deno.mkdir(distDir, { recursive: true });
 
@@ -13,10 +13,7 @@ Deno.copyFileSync(
 );
 
 await esbuild.build({
-  plugins: [...denoPlugins({
-    configPath: join(rootDir, "deno.json"),
-    lockPath: join(rootDir, "deno.lock"),
-  })],
+  plugins: [...denoPlugins()],
   entryPoints: [join(srcDir, "background.ts")],
   bundle: true,
   format: "esm",
